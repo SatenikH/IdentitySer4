25d1d9444cec84f987981c199e060d482a6bfc3b
hhar.satenik <hhar.satenik@gmail.com>
2019-09-12 11:33:03 +0300

    commit1

---
+++ WebAPIUserRegistration/Controllers/ApplicationUserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIUserRegistration.Models;
+
+namespace WebAPIUserRegistration.Controllers
+{
+    [Route("api[controller]")]
+    [ApiController]
+    public class ApplicationUserManager
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public ApplicationUserManager(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            this._userManager = userManager;
+            this._signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<Object> PostApplicationUser(ApplicationUserModel model)
+        {
+            var applicationUser = new ApplicationUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                FullName = model.FullName
+            };
+            try
+            {
+                var result = await _userManager.CreateAsync(applicationUser, model.Password);
+                return result;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
